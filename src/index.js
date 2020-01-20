1d311bef80c5262160a1ecf92bba037ed9def2be
Nevin Chen <Nevinchen@yahoo.com>
2020-01-20 02:07:10 -0500

    functionality for modal closing when clicking outside of content

--- src/index.js
+++ src/index.js
@@ -3,21 +3,15 @@ const aboutContent = document.getElementById("about");
 const aboutButton = document.getElementsByClassName("about-button")[0];
 const aboutClose = document.getElementsByClassName("about-close-content")[0];
 
-aboutButton.onclick = function() {
+aboutButton.onclick = () => {
   aboutContent.style.display = "block";
 };
 
-aboutClose.onclick = function() {
+aboutClose.onclick = () => {
   aboutContent.style.display = "none";
 };
 
-window.onclick = function(event) {
-  if (event.target == aboutContent) {
-    aboutContent.style.display = "none";
-  }
-};
-
-//projects button and Modal
+// projects button and Modal
 const projectsContent = document.getElementById("projects");
 const projectsButton = document.getElementsByClassName("projects-button")[0];
 const projectsClose = document.getElementsByClassName("projects-close-content")[0];
@@ -30,13 +24,7 @@ projectsClose.onclick = function() {
   projectsContent.style.display = "none";
 };
 
-window.onclick = function(event) {
-  if (event.target == projectsContent) {
-    projectsContent.style.display = "none";
-  }
-};
-
-//experience and education button and Modal
+// experience and education button and Modal
 const expeduContent = document.getElementsByClassName("exp-edu")[0];
 const expeduButton = document.getElementsByClassName("experience-button")[0];
 const expeduClose = document.getElementsByClassName("experience-close-content")[0];
@@ -49,8 +37,14 @@ expeduClose.onclick = function() {
     expeduContent.style.display = "none";
 };
 
-window.onclick = function(event) {
-  if (event.target == expeduContent) {
+window.onclick = function(e) {
+  if (e.target == aboutContent) {
+    aboutContent.style.display = "none";
+  }
+  if (e.target == projectsContent) {
+    projectsContent.style.display = "none";
+  }
+  if (e.target == expeduContent) {
     expeduContent.style.display = "none";
   }
 };
