8b34a745025cb64f856e07858cb53721e44b492a
Nevin Chen <Nevinchen@yahoo.com>
2020-01-21 20:49:32 -0500

    refactor compatability nav menu logic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const aboutClose = document.getElementsByClassName("about-close-content")[0];
 
 aboutButton.onclick = () => {
   aboutContent.style.display = "block";
+  closeNav();
 };
 
 aboutClose.onclick = () => {
@@ -14,30 +15,37 @@ aboutClose.onclick = () => {
 // projects button and Modal
 const projectsContent = document.getElementById("projects");
 const projectsButton = document.getElementsByClassName("projects-button")[0];
-const projectsClose = document.getElementsByClassName("projects-close-content")[0];
+const projectsClose = document.getElementsByClassName(
+  "projects-close-content"
+)[0];
 
-projectsButton.onclick = function() {
+projectsButton.onclick = () => {
   projectsContent.style.display = "block";
+  closeNav();
 };
 
-projectsClose.onclick = function() {
+projectsClose.onclick = () => {
   projectsContent.style.display = "none";
 };
 
 // experience and education button and Modal
 const expeduContent = document.getElementsByClassName("exp-edu")[0];
 const expeduButton = document.getElementsByClassName("experience-button")[0];
-const expeduClose = document.getElementsByClassName("experience-close-content")[0];
+const expeduClose = document.getElementsByClassName(
+  "experience-close-content"
+)[0];
 
-expeduButton.onclick = function() {
-    expeduContent.style.display = "block";
+expeduButton.onclick = () => {
+  expeduContent.style.display = "block";
+  closeNav();
 };
 
-expeduClose.onclick = function() {
-    expeduContent.style.display = "none";
+expeduClose.onclick = () => {
+  expeduContent.style.display = "none";
 };
 
-window.onclick = function(e) {
+//closes modal when clicking outside of content
+window.onclick = e => {
   if (e.target == aboutContent) {
     aboutContent.style.display = "none";
   }
@@ -47,4 +55,33 @@ window.onclick = function(e) {
   if (e.target == expeduContent) {
     expeduContent.style.display = "none";
   }
+};
+
+//collapse button for responsive nav menu
+const collapseButton = document.getElementById("collapse-menu");
+const navMenu = document.getElementsByClassName("nav-items")[0];
+
+const openNav = () => {
+  if (navMenu.classList[1] === "close") navMenu.classList.remove("close");
+  navMenu.classList.add("open");
+};
+
+const closeNav = () => {
+  navMenu.classList.remove("open");
+  navMenu.classList.add("close");
+};
+
+collapseButton.onclick = () => {
+  if (navMenu.classList[1] === undefined || navMenu.classList[1] === "close") {
+    openNav();
+  } else if (navMenu.classList[1] === "open") {
+    closeNav();
+  }
+};
+
+// contact nav button for collapse
+const contactButton = document.getElementsByClassName("contact-button")[0];
+
+contactButton.onclick = () => {
+  closeNav();
 };
