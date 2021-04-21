89d1711fb29882444d80ca5804edf66af1c1648d
Nevin Chen <Nevinchen@yahoo.com>
2021-04-21 13:50:11 -0400

    updates to portfolio page content and css

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
+    entry: './assets/js/main.js',
     output: {
-      path: path.join(__dirname + '/public'),
+      path: path.join(__dirname),
       filename: 'bundle.js' 
     },
     devtool: 'source-map'
